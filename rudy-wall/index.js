d90feb9e39e87eb09e81a69478e37ba8a1c99e54
Hoppy <mark82hopcraft@gmail.com>
2021-06-30 23:36:04 +0100

    1/3 methods done

--- rudy-wall/index.js
+++ rudy-wall/index.js
@@ -1,68 +1,111 @@
-/* Cfg */
-// const baseImagesJSONPath = './src/data/gallery-images.json';
-// const imageOutputConfigs = [
-// 	{ width: 300, quality:  80, destPath: './dist/assets/images/rudy-300' },
-// 	{ width: 480, quality:  80, destPath: './dist/assets/images/rudy-480' },
-// 	{ width: 800, quality:  80, destPath: './dist/assets/images/rudy-800' },
-// 	{ width: 1200, quality:  80, destPath: './dist/assets/images/rudy-1200' }
-// ];
-// const imagesMetaDestPath = './dist/data/imagesMeta.json';
-
-
-// const itemCustomPropertyName = "rudyCard";
-// const itemCssClass = "rudy-cards__card";
-// const cssGutterWidthVarName = '--gutter';
-// const cssBorderWidthVarName = '--borderWidth';
-// const cssPaddingWidthVarName = '--paddingWidth';
-
-// const outputPath = "./dist/styles/built-rudy-gallery.css";
-// const breakpoints = [340, 600, 980];
-// const patterns = [
-// 	[1, 2],
-// 	[2, 3],
-// 	[3, 4, 5]
-// ];
-
-// INPUT
-
-//OUTPUT
-// optimized images in sub dirs
-// json meta
-
-
-// INPUT
-// json meta
-// patterns
-// bps
-// css stuff
-
-//OUTPUT
-// css file with custom properties & media queries
-
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
 
 export default class RudyWall {
 	constructor({
 		baseImagePaths,
-		outputImages,
-		outputMeta,
+		imageOutputConfigs,
 		breakpoints,
-		patterns
+		patterns,
+		imageFilesInfo,
+		cssClass,
+		cssGutterWidthName,
+		cssBorderWidthName,
+		cssPaddingWidthName
 	} = {
-		baseImagePaths: [],
-		outputImages: [
-			{ width: 300, quality: 80, destPath: './rw-default-output/images/rw300'},
-			{ width: 600, quality: 80, destPath: './rw-default-output/images/rw600'}
+		baseImagePaths: null,
+		imageOutputConfigs: [
+			{ width: 300, quality: 80, destPath: path.resolve('./rw-default-output/rw300') },
+			{ width: 600, quality: 80, destPath: path.resolve('./rw-default-output/rw600') }
 		],
-		outputMeta: './rw-default-output/meta.json',
 		breakpoints: [340, 600, 980],
-		patterns: [ [1,2], [2,3], [3,4,5] ]
+		patterns: [ [1,2], [2,3], [3,4,5] ],
+		imageFilesInfo: null,
+		cssClass: 'rw-card',
+		cssGutterWidthName: 'rw-card-gutter-width',
+		cssBorderWidthName: 'rw-card-border-width',
+		cssPaddingWidthName: 'rw-card-padding-width'
 	})
 	{
 		this.baseImagePaths = baseImagePaths;
-		this.outputImages = outputImages;
+		this.imageOutputConfigs = imageOutputConfigs;
 		this.breakpoints = breakpoints;
 		this.patterns = patterns;
+		this.imageFilesInfo = imageFilesInfo;
+		this.cssClass = cssClass,
+		this.cssGutterWidthName = cssGutterWidthName,
+		this.cssBorderWidthName = cssBorderWidthName,
+		this.cssPaddingWidthName = cssPaddingWidthName
 	}
 
 
+	/*
+	 * Writes new images of size and quality determined by imageOutputConfigs
+	 */
+	async generateImages() {
+		const promises = [];
+		const imageFilesInfo = [];
+ 
+		for (const baseImagePath of this.baseImagePaths) {
+			const imageFileInfo = {
+				aspect: null,
+				sizes: []
+			}
+
+			sharp(baseImagePath)
+				.metadata()
+				.then(metadata => {
+					imageFileInfo.aspect = metadata.width / metadata.height;
+				});
+
+			for (const cfg of this.imageOutputConfigs) {
+				promises.push(new Promise((resolve, reject) => {
+					const ext = path.extname(baseImagePath);
+					const fName = path.basename(baseImagePath, ext).split(' ').join('');
+					const newFileName = `${fName}-${cfg.width}${ext}`;
+					const outputPath = path.resolve( `${cfg.destPath}/${newFileName}` );
+
+					if (!fs.existsSync(cfg.destPath))
+						fs.mkdirSync(cfg.destPath, { recursive: true });
+
+					sharp(baseImagePath)
+						.resize(cfg.width)
+						.jpeg({ mozjpeg: true, quality: cfg.quality })
+						.toFile(outputPath)
+						.then(info => {
+							imageFileInfo.sizes.push({
+								width: info.width,
+								height: info.height,
+								path: outputPath
+							});
+							resolve();
+						})
+
+						.catch(err => { reject(err); });
+				}));
+			}
+			imageFilesInfo.push(imageFileInfo);
+		}
+
+		return Promise.all(promises)
+			.then( () => imageFilesInfo );
+	}
+
+
+	/*
+	 *
+	 * Uses (this.imageFilesInfo, this.patterns)
+	 */
+	getImageWidthsAtPatterns() {
+		console.log('getImageWidthsAtPatterns..');
+	}
+
+
+	/*
+	 * 
+	 */
+	getCss() {
+		console.log('getCss..');
+	}
 }
--- test-rudy-wall.js
+++ test-rudy-wall.js
@@ -1,10 +1,67 @@
+import fs from 'fs';
+import path from 'path';
 import rudyWall from "./rudy-wall/index.js";
 
+/* Config */
+const baseImagesJSONPath = './src/data/gallery-images.json';
+const imageOutputConfigs = [
+	{ width: 300, quality:  80, destPath: path.resolve('./dist2/images/rw300') },
+	{ width: 480, quality:  80, destPath: path.resolve('./dist2/images/rw480') },
+	{ width: 800, quality:  80, destPath: path.resolve('./dist2/images/rw800') },
+	{ width: 1200, quality:  80, destPath: path.resolve('./dist2/images/rw1200') }
+]
+const imageFilesInfoPath = path.resolve('./dist2/data/imageFilesInfo.json');
 
-const rudyWall1 = new rudyWall();
-
-
-for (const prop in rudyWall1) {
-  console.log(`${prop}: ${rudyWall1[prop]}`);
+/* Vars */
+const baseImagesJSON = fs.readFileSync(baseImagesJSONPath);
+const baseImagePaths = JSON.parse(baseImagesJSON).images.map(image => image.url);
+const options = {
+	baseImagePaths: baseImagePaths,
+	imageOutputConfigs: imageOutputConfigs
 }
 
+/* Init */
+const rudyWall1 = new rudyWall(options);
+
+// Example
+// const imageFilesInfo = rudyWall1.generateImages(baseImagePaths, imageOutputConfig);
+// const imageWidthsAtPatterns = rudyWall1.imageWidthsAtPatterns( imageFilesInfo, patterns )
+// const { cssCustomProps, mediaQueries } = rudyWall1.generateCss( breakpoints, cssStuff)
+
+/* Run */
+(async () => {
+	rudyWall1.imageFilesInfo = await getImageFilesInfo(rudyWall1, imageFilesInfoPath);
+
+	rudyWall1.imageFilesInfo.forEach(imageInfo => {
+		//console.log(JSON.stringify(imageInfo, null, 2));
+	});
+
+	rudyWall1.getImageWidthsAtPatterns();
+	rudyWall1.getCss();
+})();
+
+
+
+/*
+ * Checks existance of json file to determine whether to generate images or
+ * load image file info from json file.
+ * Creates the json file after generating the images.
+ * - Leave json file for quicker build (if images unchanged).
+ * - Delete json file to re-build images (if images or cfgs changed).
+ */
+async function getImageFilesInfo(rudyWall, imageFilesInfoPath) {
+	let imagesInfo = null;
+
+	if ( fs.existsSync(imageFilesInfoPath) ) {
+		let imagesInfoJSON = fs.readFileSync(imageFilesInfoPath);
+		imagesInfo = JSON.parse(imagesInfoJSON);
+	}
+	else {
+		imagesInfo = await rudyWall.generateImages();
+		if ( !fs.existsSync( path.dirname(imageFilesInfoPath)) )
+			fs.mkdirSync( path.dirname(imageFilesInfoPath), { recursive: true } );
+		fs.writeFileSync( imageFilesInfoPath, JSON.stringify(imagesInfo, null, 2) );
+	}
+
+	return imagesInfo;
+}
